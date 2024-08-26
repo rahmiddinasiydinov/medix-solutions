59ce465e38efa2ce056680f4ec60cbc60dd504ca
rahmiddinasiydinov <rahmiddinasiydinov@gmail.com>
2024-08-26 22:36:22 +0500

    remove particles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.scss'
 import Countdown from './Components/Countdown/Countdown'
 import Header from './Components/Header/Header'
-// import { Particle } from './Components/Particle/Particle'
 
 function App() {
   return (
     <>
-    {/* <Particle/> */}
       <div>
           <Header/>
           <Countdown/>
